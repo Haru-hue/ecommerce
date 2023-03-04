3efb9ee5572a8ce782c77b8c32916117e8f81794
Haru-hue <53390409+Haru-hue@users.noreply.github.com>
2023-03-04 19:51:25 +0100

    Added CSS for Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,21 +4,37 @@ import Logo from '../images/Logo.png'
 export default function Navbar () {
     const location = useLocation()
     const isHidden = location.pathname.startsWith('/user') ? 'hidden' : ''
+
     return (
         <header className={`container-fluid ${isHidden}`}>
-            <nav className="navbar navbar-expand-lg p-3">
-                <div className="navbar-brand">
+            <nav className="navbar navbar-expand-lg p-5 py-2">
+                <div className="navbar-brand px-5">
                         <a href="/"><img src={Logo} alt="FarmHub" className="img-fluid" /></a>
                 </div>
-                <form action="" className="d-flex" role="search">
-                        <input className="form-control" type="search" name="" id="" placeholder="Search for products here"/>
-                        <i class="fa-solid fa-magnifying-glass"></i>
+                <form action="" className="d-flex ps-5" role="search">
+                    <div className="search-field">
+                        <span>
+                            <select name="" id="">
+                                <option selected>All Categories</option>
+                                <option>Fruits & Vegetables</option>
+                                <option>Frozen Seafoods</option>
+                                <option>Dairy Products</option>
+                                <option>Wheat and Grains</option>
+                                <option>Bread and Pastries</option>
+                                <option>Raw Meats</option>
+                            </select>
+                        </span>
+                        <div className="input-group">
+                            <input className="form-control" type="search" name="" id="" placeholder="Search for products here"/>
+                            <div className="input-group-text"><i class="fa-solid fa-magnifying-glass"></i></div>
+                        </div>
+                    </div>
                 </form>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav navbar-icons">
+                    <ul className="navbar-nav ms-auto navbar-icons d-flex">
                         <li className="nav-item"><i class="fa-regular fa-user"></i></li>
                         <li className="nav-item"><i class="fa-regular fa-heart"></i></li>
                         <li className="nav-item"><i class="fa-solid fa-circle-question"></i></li>
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -47,13 +47,31 @@ export default function Homepage () {
             <section className="deals-section p-5">
                 <span className="heading">
                     <h1>Popular Deals | </h1>
-                    <p>See All {'->'}</p>
+                    See All {'->'}
                 </span>
                 <div className="deals-grid">
-                    <div>1</div>
-                    <div>2</div>
-                    <div>3</div>
-                    <div>4</div>
+                    <div className="deal-box">
+                        <h2>100% Pure Orange Juice</h2>
+                        <p>Fresh orange juice with no added preservatives</p>
+                    </div>
+                    <div className="deal-box">
+                        <span>
+                            <h2>₦2,500.00</h2>
+                            <s>₦4,500.00</s>
+                        </span>
+                        <h6>CANNED FISH SALMON</h6>
+                        <p>250g</p>
+                    </div>
+                    <div className="deal-box">
+                        <h2>whole milk</h2>
+                        <p>Fresh milk and lactose-free providing all the ingredients</p>
+                        <h3>ONLY
+                        ₦1,800.00</h3>
+                    </div>
+                    <div className="deal-box">
+                        <h2>Wheat Bread</h2>
+                        <p>Whole-grain wheat bread, freshly-baked</p>
+                    </div>
                 </div>
             </section>
             <section className="bg-section">
@@ -71,11 +89,38 @@ export default function Homepage () {
                     <div className="row">
                         <div className="col-lg-7">
                             <div className="d-flex flex-column">
-                                <div className="deals-box">1</div>
-                                <div className="deals-box">2</div>
+                                <div className="deals-box">
+                                    <div className="deal-text">
+                                        <h3>YOUR EVERYDAY ITEMS DELIVERED AT YOUR DOORSTEP</h3>
+                                        <h1>For Free</h1>
+                                        <p>Terms and Conditions Apply</p>
+                                    </div>
+                                </div>
+                                <div className="deals-box">
+                                    <h3>GROCERY SUPER DEALS</h3>
+                                    <h5>Save more on your groceries</h5>
+                                </div>
                             </div>
                         </div>
-                        <div className="col-lg-5">User Sign Up</div>
+                        <div className="col-lg-5">
+                            <h2>15% OFF</h2>
+                            <h3>For new users, sign up for this exclusive promo</h3>
+                            <form action="" method="post">
+                                <div className="input-group">
+                                    <span className="input-group-text"><i className="fa-regular fa-envelope"></i></span>
+                                    <input type="email" name="" id="" placeholder="Email address" className="form-control"/>
+                                </div>
+                                <div className="input-group">
+                                    <span className="input-group-text"><i className="fa-solid fa-lock"></i></span>
+                                    <input type="password" name="" id="" placeholder="Password" className="form-control"/>
+                                </div>
+                                <div className="input-group">
+                                    <span className="input-group-text"><i className="fa-solid fa-repeat"></i></span>
+                                    <input type="password" name="" id="" placeholder="Confirm Password" className="form-control"/>
+                                </div>
+                                <button type="submit" className="btn-register">Register Now</button>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </section>
