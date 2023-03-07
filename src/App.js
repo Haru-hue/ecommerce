311450d0a53c2bee22b967b5a4b60bf9fc6e3a3c
Bence Fekete <fekete965@gmail.com>
2023-03-07 20:49:47 +0000

    Update legacy routing

--- src/App.js
+++ src/App.js
@@ -1,37 +1,11 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
 
-import Footer from "./components/Footer";
-import Homepage from "./pages/Homepage";
-import Components from "./pages/Components";
-import Navbar from "./components/Navbar";
-import Modal from "./pages/SignInModal";
-import ProductPage from "./views/ProductPage";
-import UserProfile from "./views/UserProfile";
-
-// import "normalize.css";
+import "normalize.css";
 import "./App.scss";
 
 function App() {
-  return (
-    <>
-      <Router basename="/">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Homepage />} exact />
-          <Route path="/user/login" element={<Modal />} exact />
-
-          {/* Product routes */}
-          <Route path="/products" element={<ProductPage />} exact />
-          <Route path="/products" element={<ProductPage />} exact />
-          <Route path="/profiles/:userId" element={<UserProfile />} exact />
-
-          {/* Dummy route to showcase component */}
-          <Route path="/components" element={<Components />} exact />
-        </Routes>
-        <Footer />
-      </Router>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
