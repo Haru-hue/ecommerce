57b001a8de30edab18ddc94d06b4401b8da1f5fb
Bence Fekete <fekete965@gmail.com>
2023-03-10 23:26:51 +0000

    Add friendly paths for imports

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,18 @@
 import react from '@vitejs/plugin-react'
+import path from 'node:path'
 import { defineConfig, splitVendorChunkPlugin } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), splitVendorChunkPlugin()],
+  resolve: {
+    alias: {
+      api: path.resolve(__dirname, 'src', 'api'),
+      assets: path.resolve(__dirname, 'src', 'assets'),
+      components: path.resolve(__dirname, 'src', 'components'),
+      router: path.resolve(__dirname, 'src', 'router'),
+      routes: path.resolve(__dirname, 'src', 'routes'),
+      style: path.resolve(__dirname, 'src', 'style'),
+    },
+  },
 })
