23045057df6410295214f3f0e2f167c7394f55a7
Bence Fekete <fekete965@gmail.com>
2023-03-15 10:36:56 +0000

    Connect to mongoose db properly

--- backend/models/db.js
+++ backend/models/db.js
@@ -3,9 +3,17 @@ require('dotenv').config()
 
 const MONGODB_URI = process.env.MONGODB_URI
 
-mongoose.connect('mongodb://127.0.0.1:27017', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((err) => console.error('Error connecting to MongoDB', err));
+if (!MONGODB_URI) {
+  throw new Error('MONGODB_URI must be defined')
+}
+
+const connectToMongoose = () => {
+  return mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err) => console.error('Error connecting to MongoDB', err));
+}
+
+module.exports = connectToMongoose
