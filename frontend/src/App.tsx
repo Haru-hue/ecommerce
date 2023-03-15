170823c6502786b047741e702d4df84844f3ac63
Bence Fekete <fekete965@gmail.com>
2023-03-15 10:13:06 +0000

    Add AuthProvider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import { Suspense } from 'react'
 import { RouterProvider } from 'react-router-dom'
 
+import AuthProvider from './providers/AuthProvider'
+
 import Loader from 'components/loader'
 
 import './App.scss'
@@ -10,7 +12,9 @@ import 'reset-css'
 
 const App = () => (
   <Suspense fallback={<Loader fixed />}>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </Suspense>
 )
 
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
       api: path.resolve(__dirname, 'src', 'api'),
       assets: path.resolve(__dirname, 'src', 'assets'),
       components: path.resolve(__dirname, 'src', 'components'),
+      providers: path.resolve(__dirname, 'src', 'providers'),
       router: path.resolve(__dirname, 'src', 'router'),
       routes: path.resolve(__dirname, 'src', 'routes'),
       style: path.resolve(__dirname, 'src', 'style'),
