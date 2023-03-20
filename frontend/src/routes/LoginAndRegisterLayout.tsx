612eb3804ed424f06531af71f4902d4801e9fad5
Bence Fekete <fekete965@gmail.com>
2023-03-20 10:31:30 +0000

    Redirect logged in users from login and register page

--- frontend/src/routes/LoginAndRegisterLayout.tsx
+++ frontend/src/routes/LoginAndRegisterLayout.tsx
@@ -1,14 +1,24 @@
-import { Outlet } from 'react-router-dom'
+import { Navigate, Outlet } from 'react-router-dom'
+
+import { useAuthValues } from 'providers/AuthProvider'
 
 import Copyright from 'components/Copyright'
 
 import styles from './LoginAndRegisterLayout.module.scss'
 
-const LoginAndRegisterLayout = () => (
-  <div className={styles.container}>
-    <Outlet />
-    <Copyright />
-  </div>
-)
+const LoginAndRegisterLayout = () => {
+  const { user } = useAuthValues()
+
+  if (user) {
+    return <Navigate to="/" />
+  }
+
+  return (
+    <div className={styles.container}>
+      <Outlet />
+      <Copyright />
+    </div>
+  )
+}
 
 export default LoginAndRegisterLayout
